d6c833f5c6b184e64764f0adc7bfa1501e442e46
Pedro Bacelar <pedrobacelarsantos@usp.br>
2024-04-26 11:04:31 +0100

    correct api

--- main_chat_app.py
+++ main_chat_app.py
@@ -170,8 +170,7 @@ time_sleep_longer = 1
 #    st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 #--- Replicate API Settings & llama2
-#replicate_api = st.secrets['REPLICATE_API_TOKEN']
-replicate_api = "r8_dy9Cxc78exa0IhYwtH4TTuGpxIZ8tc33WJSxp"
+replicate_api = st.secrets['REPLICATE_API_TOKEN']
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 llama2 = Llama2()
 
